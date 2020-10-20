b838788f178aed7a4a680dc9b832454074a6dd15
Sky24H <hytian2@gmail.com>
2020-10-20 11:31:04 +0900

    update

--- check.py
+++ check.py
@@ -1,21 +1,17 @@
 import os
-a = './results/'
-b = './datasets/flickr/'
-count = 0
-list_a = []
-for root,dirs,files in os.walk(a):
-	for name in files:
-		list_a.append(name[:len(name)-4])
+import shutil
 
-for i in range(10):
-	for root,dirs,fff in os.walk(b+str(i)+"/"):
-		for name in fff:
-			if name[:len(name)-4] in list_a:
-				s = b+str(i)+"/"+name
-				d = "./done/"+name
-				os.rename(s,d)
-				print('ok')
-				count += 1
-			else:
-				print("ng")
-print(count)
+a = './a/'
+b = './b/'
+bx = './x/'
+
+list_a = os.listdir(a)
+list_b = os.listdir(b)
+count = 0
+
+for filename in list_b:
+    if filename in list_a:
+        #print('ok')
+        #shutil.copy(b+filename, x+filename)
+        count += 1
+print(count, '/'+len(list_b))
