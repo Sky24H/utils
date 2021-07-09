cf3680a7457edd066cad38233ab6c05b7f15f18d
Sky24H <hytian2@gmail.com>
2021-07-09 18:19:36 +0900

    update

--- flickr_downloader/flickr.py
+++ flickr_downloader/flickr.py
@@ -2,40 +2,50 @@ from flickrapi import FlickrAPI
 from urllib.request import urlretrieve
 import os, time, sys
 
-# 「事前準備」で取得したAPI KeyとSecret Keyを設定
+# API KeyとSecret Key
 key = "e2285a17d43facdec501606eb1434e21"
 secret = "660af1b75058b9db"
 
-# 1秒間隔でデータを取得(サーバー側が逼迫するため)
-wait_time = 1.1
+# wait for 1.1s due to API limitations.
+wait_time = 1
 
-# 検索キーワード(実行時にファイル名の後に指定)
+# search keyword
 keyword = sys.argv[1]
-# 保存フォルダ
-savedir = "./" + keyword + "/"
-count = 45000
-count_page = 91
-# 接続クライアントの作成とサーチの実行
+savedir = "./" + keyword
+os.makedirs(savedir, exist_ok=True)
+
 flickr = FlickrAPI(key, secret, format='parsed-json')
-for i in range(11):
-    result = flickr.photos.search(
-        text = keyword,           # 検索キーワード
-        per_page = 500,           # 取得データ数
-        page = count_page,
-        media = 'photos',         # 写真を集める
-        sort = 'relevance',       # 最新のものから取得
-        safe_search = 1,          # 暴力的な画像を避ける
-        extras = 'url_n, license' # 余分に取得する情報(ダウンロード用のURL、ライセンス)
-    )
-    photos = result['photos']
-    for i, photo in enumerate(photos['photo']):
-        url_n = photo['url_n']
-        filepath = savedir + "{:0>2d}".format(count_page) + photo['id'] + '.jpg'
-        count += 1
-        print("now in page "+"{:0>2d}".format(count_page)+", total counts for images :"+str(count)+"/50000")
-        if os.path.exists(filepath):
-            pass
-        else:
-            urlretrieve(url_n, filepath)
-        time.sleep(wait_time)
+count_page = 0
+
+for i in range(20):
     count_page += 1
+
+    result = flickr.photos.search(
+        text = keyword,
+        per_page = 250,
+        media = 'photos',
+        sort = 'relevance',
+        # set limitations here to obtain more results.
+        # min_upload_date = '1283228800',
+        min_upload_data = '1262304000',
+
+        safe_search = 1,
+        extras = 'url_l, license',
+        page = count_page,
+        geo_context = 2,
+    )
+
+    # get reusult and save it.
+    photos = result['photos']
+    count = 0
+    for i, photo in enumerate(photos['photo']):
+        try:
+            url = photo['url_l']
+        except KeyError:
+            continue
+        count += 1
+        print(count)
+        filepath = savedir + '/' + photo['id'] + '.jpg'
+        #if os.path.exists(filepath): continue
+        urlretrieve(url, filepath)
+        time.sleep(wait_time)
